626fc83d437050694b218cd69e3422b73ea7c26e
Maame Yaa Serwaa Bona-Mensa <85899285+mbonamensa@users.noreply.github.com>
2021-12-24 18:04:30 +0000

    Add files via upload
    File uploads

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function dumbFunction() {
+  var changeDumb = "Whoops, I made a mistake. Mistakes are human";
+  document.getElementById("re-dumb").innerHTML = changeDumb;
+  document.getElementById("dumb").innerHTML;
+  dumb.style.textDecoration = "line-through";
+}
+
+function goodenoughFunction() {
+  var changeGoodenough = "I am enough";
+  document.getElementById("re-goodenough").innerHTML = changeGoodenough;
+  document.getElementById("goodenough").innerHTML;
+  goodenough.style.textDecoration = "line-through";
+}
+
+function messFunction() {
+  var changeMess = "I am human.";
+  document.getElementById("re-mess").innerHTML = changeMess;
+  document.getElementById("mess").innerHTML;
+  mess.style.textDecoration = "line-through";
+}
+
+function failureFunction() {
+  var changeFailure = "This is really hard and I am going to keep trying";
+  document.getElementById("re-failure").innerHTML = changeFailure;
+  document.getElementById("failure").innerHTML;
+  failure.style.textDecoration = "line-through";
+}
+
+function shyFunction() {
+  var changeShy = "I believe in myself";
+  document.getElementById("re-shy").innerHTML = changeShy;
+  document.getElementById("shy").innerHTML;
+  shy.style.textDecoration = "line-through";
+}
